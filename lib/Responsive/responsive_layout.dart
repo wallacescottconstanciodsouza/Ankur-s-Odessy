71afd0c89e289664f1dac861ccd0ac6c1249e87b
wallace <wallacedso42@gmail.com>
2022-09-13 13:56:57 +0530

    Business profile and auth me n ankur

---
+++ lib/Responsive/responsive_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mk2/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({Key? key, required this.mobileScreenLayout, required this.webScreenLayout}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreensize) {
+          //web screen
+          return webScreenLayout;
+        }
+        //mobile screen
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
